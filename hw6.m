8004ffa351c0310850967b884ac36b2805d67e0f
Fengan Li <fengan@espresso.cs.wisc.edu>
2016-03-08 20:10:51 -0600

    add 730 hw6

---
+++ hw6.m
@@ -0,0 +1,36 @@
+global chi
+chi = rand(100, 1)*2-1;
+x=rand(100, 1);
+y = rand(100, 1);
+rho = 1;
+z = 1;
+iters = 1;
+while 1
+    iters = iters + 1;
+    % The function has the close form, thus we solve them directly
+    x_new =zeros(100, 1);
+    for i = 1:100
+        x_new(i) = (chi(i)-y(i)+rho*z) / (1+rho);
+    end
+    z_new = 0;
+    for i = 1:100
+        z_new = z_new+(x_new(i)+1/rho*y(i));
+    end
+    z_new= z_new/100;
+    y_new = zeros(100, 1);
+    for i = 1:100
+        y_new(i) = y(i) + rho*(x_new(i)-z_new);
+    end
+    x = x_new;
+    y = y_new;
+    z = z_new;
+    kkt_norm = 0;
+    kkt_norm = norm(x-chi+y) + norm(sum(y)) + norm(x-z);
+    if (kkt_norm) < 1e-8
+        break;
+    end
+end
+iters
+x
+z
+mean(chi)
